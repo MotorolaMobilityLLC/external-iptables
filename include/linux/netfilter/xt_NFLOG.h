59f7fe45252902a578315924b5fdb4f1e4bc58fb
Yu Miao <miaoyu4@lenovo.com>
2017-09-13 14:06:55 +0800

    IKZUIPRC-1 iptables: support layered NFLOG
    Add --nflog-layer NUM parameter.
    Param:
    0: all(default)
    1: ip header only
    2: ip header and transport header
    3: transport header only
    4: transport header and application data
    5: application data only
    
    Note that this option only cares about TCP
    and UDP transport layer protocol.
    
    Add --notify for tlv module. It will only
    send notification to userspace when 'notify'
    is set.
    
    Change-Id: Ib6bf613bd00eb805493b7bc3b28330aff09a5666
    Signed-off-by: Yu Miao <miaoyu4@lenovo.com>
    Signed-off-by: Hongjie Liu <liuhj7@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1057575
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xu Gao <njh348@motorola.com>
    Reviewed-by: Yong Zhang <yongzhang@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/netfilter/xt_NFLOG.h
+++ include/linux/netfilter/xt_NFLOG.h
@@ -6,6 +6,14 @@
 #define XT_NFLOG_DEFAULT_GROUP		0x1
 #define XT_NFLOG_DEFAULT_THRESHOLD	0
 
+#define NF_LOG_ALL                  0x00 //copy network plus trasnport plus app data
+#define NF_LOG_NETWORK_ONLY         0x01 //only copy network data
+#define NF_LOG_NETWORK_TRANSPORT    0x02 //copy network plus transport
+#define NF_LOG_TRANSPORT_ONLY       0x03 //only copy transport data
+#define NF_LOG_TRANSPORT_APP        0x04 //copy transport plus app data
+#define NF_LOG_APP_ONLY             0x05 //only copy app data
+#define NF_LOG_DEFAULT_LAYER        NF_LOG_ALL
+
 #define XT_NFLOG_MASK			0x1
 
 /* This flag indicates that 'len' field in xt_nflog_info is set*/
@@ -19,6 +27,7 @@ struct xt_nflog_info {
 	__u16	flags;
 	__u16	pad;
 	char		prefix[64];
+	__u16   layer;
 };
 
 #endif /* _XT_NFLOG_TARGET */

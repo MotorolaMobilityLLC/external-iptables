ae4160391a312e2c6f48bd1b9a282db6b5d902b5
Yu Miao <miaoyu4@lenovo.com>
2017-10-13 17:02:43 +0800

    iptables: add NFLOG v1 for compatibility
    As 'layer' param added, we should increase
    the revision number to avoid compatibility issue.
    
    Change-Id: I2a090710d6c78cc12c824ed37e9df4a0dc707ac1
    Signed-off-by: Yu Miao <miaoyu4@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1073754
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hongjie Liu <liuhj7@lenovo.com>
    Reviewed-by: Wei Zhang <e7976c@motorola.com>
    Reviewed-by: Yong Zhang <yongzhang@motorola.com>
    Submit-Approved: Jira Key

--- extensions/libxt_NFLOG.c
+++ extensions/libxt_NFLOG.c
@@ -23,8 +23,6 @@ enum {
 static const struct xt_option_entry NFLOG_opts[] = {
 	{.name = "nflog-group", .id = O_GROUP, .type = XTTYPE_UINT16,
 	 .flags = XTOPT_PUT, XTOPT_POINTER(s, group)},
-	{.name = "nflog-layer", .id = O_LAYER, .type = XTTYPE_UINT16,
-	 .flags = XTOPT_PUT, XTOPT_POINTER(s, layer)},
 	{.name = "nflog-prefix", .id = O_PREFIX, .type = XTTYPE_STRING,
 	 .min = 1, .flags = XTOPT_PUT, XTOPT_POINTER(s, prefix)},
 	{.name = "nflog-range", .id = O_RANGE, .type = XTTYPE_UINT32,
@@ -37,7 +35,35 @@ static const struct xt_option_entry NFLOG_opts[] = {
 };
 #undef s
 
+#define s struct xt_nflog_info_v1
+static const struct xt_option_entry NFLOG_opts_v1[] = {
+	{.name = "nflog-group", .id = O_GROUP, .type = XTTYPE_UINT16,
+	 .flags = XTOPT_PUT, XTOPT_POINTER(s, group)},
+	{.name = "nflog-prefix", .id = O_PREFIX, .type = XTTYPE_STRING,
+	 .min = 1, .flags = XTOPT_PUT, XTOPT_POINTER(s, prefix)},
+	{.name = "nflog-range", .id = O_RANGE, .type = XTTYPE_UINT32,
+	 .excl = F_SIZE, .flags = XTOPT_PUT, XTOPT_POINTER(s, len)},
+	{.name = "nflog-size", .id = O_SIZE, .type = XTTYPE_UINT32,
+	 .excl = F_RANGE, .flags = XTOPT_PUT, XTOPT_POINTER(s, len)},
+	{.name = "nflog-threshold", .id = O_THRESHOLD, .type = XTTYPE_UINT16,
+	 .flags = XTOPT_PUT, XTOPT_POINTER(s, threshold)},
+	{.name = "nflog-layer", .id = O_LAYER, .type = XTTYPE_UINT16,
+	 .flags = XTOPT_PUT, XTOPT_POINTER(s, layer)},
+	XTOPT_TABLEEND,
+};
+#undef s
+
 static void NFLOG_help(void)
+{
+	printf("NFLOG target options:\n"
+	       " --nflog-group NUM		NETLINK group used for logging\n"
+	       " --nflog-range NUM		This option has no effect, use --nflog-size\n"
+	       " --nflog-size NUM		Number of bytes to copy\n"
+	       " --nflog-threshold NUM		Message threshold of in-kernel queue\n"
+	       " --nflog-prefix STRING		Prefix string for log messages\n");
+}
+
+static void NFLOG_help_v1(void)
 {
 	printf("NFLOG target options:\n"
 	       " --nflog-group NUM		NETLINK group used for logging\n"
@@ -57,6 +83,13 @@ static void NFLOG_help(void)
 static void NFLOG_init(struct xt_entry_target *t)
 {
 	struct xt_nflog_info *info = (struct xt_nflog_info *)t->data;
+
+	info->threshold	= XT_NFLOG_DEFAULT_THRESHOLD;
+}
+
+static void NFLOG_init_v1(struct xt_entry_target *t)
+{
+	struct xt_nflog_info_v1 *info = (struct xt_nflog_info_v1 *)t->data;
 
 	info->threshold	= XT_NFLOG_DEFAULT_THRESHOLD;
 	info->layer	= NF_LOG_DEFAULT_LAYER;
@@ -84,6 +117,18 @@ static void NFLOG_check(struct xt_fcheck_call *cb)
 
 	if (cb->xflags & F_SIZE)
 		info->flags |= XT_NFLOG_F_COPY_LEN;
+}
+
+static void NFLOG_check_v1(struct xt_fcheck_call *cb)
+{
+	struct xt_nflog_info_v1 *info = cb->data;
+
+	if (cb->xflags & F_RANGE)
+		fprintf(stderr, "warn: --nflog-range has never worked and is no"
+			" longer supported, please use --nflog-size insted\n");
+
+	if (cb->xflags & F_SIZE)
+		info->flags |= XT_NFLOG_F_COPY_LEN;
 
 	if (info->layer < 0 || info->layer > 5)
 		fprintf(stderr, "warn: --nflog-layer param value out of range, "
@@ -91,6 +136,22 @@ static void NFLOG_check(struct xt_fcheck_call *cb)
 }
 
 static void nflog_print(const struct xt_nflog_info *info, char *prefix)
+{
+	if (info->prefix[0] != '\0') {
+		printf(" %snflog-prefix ", prefix);
+		xtables_save_string(info->prefix);
+	}
+	if (info->group)
+		printf(" %snflog-group %u", prefix, info->group);
+	if (info->flags & XT_NFLOG_F_COPY_LEN)
+		printf(" %snflog-size %u", prefix, info->len);
+	else if (info->len)
+		printf(" %snflog-range %u", prefix, info->len);
+	if (info->threshold != XT_NFLOG_DEFAULT_THRESHOLD)
+		printf(" %snflog-threshold %u", prefix, info->threshold);
+}
+
+static void nflog_print_v1(const struct xt_nflog_info_v1 *info, char *prefix)
 {
 	if (info->prefix[0] != '\0') {
 		printf(" %snflog-prefix ", prefix);
@@ -116,11 +177,26 @@ static void NFLOG_print(const void *ip, const struct xt_entry_target *target,
 	nflog_print(info, "");
 }
 
+static void NFLOG_print_v1(const void *ip, const struct xt_entry_target *target,
+			int numeric)
+{
+	const struct xt_nflog_info_v1 *info = (struct xt_nflog_info_v1 *)target->data;
+
+	nflog_print_v1(info, "");
+}
+
 static void NFLOG_save(const void *ip, const struct xt_entry_target *target)
 {
 	const struct xt_nflog_info *info = (struct xt_nflog_info *)target->data;
 
 	nflog_print(info, "--");
+}
+
+static void NFLOG_save_v1(const void *ip, const struct xt_entry_target *target)
+{
+	const struct xt_nflog_info_v1 *info = (struct xt_nflog_info_v1 *)target->data;
+
+	nflog_print_v1(info, "--");
 }
 
 static void nflog_print_xlate(const struct xt_nflog_info *info,
@@ -141,6 +217,26 @@ static void nflog_print_xlate(const struct xt_nflog_info *info,
 	xt_xlate_add(xl, "group %u ", info->group);
 }
 
+static void nflog_print_xlate_v1(const struct xt_nflog_info_v1 *info,
+			      struct xt_xlate *xl, bool escape_quotes)
+{
+	xt_xlate_add(xl, "log ");
+	if (info->prefix[0] != '\0') {
+		if (escape_quotes)
+			xt_xlate_add(xl, "prefix \\\"%s\\\" ", info->prefix);
+		else
+			xt_xlate_add(xl, "prefix \"%s\" ", info->prefix);
+
+	}
+	if (info->flags & XT_NFLOG_F_COPY_LEN)
+		xt_xlate_add(xl, "snaplen %u ", info->len);
+	if (info->threshold != XT_NFLOG_DEFAULT_THRESHOLD)
+		xt_xlate_add(xl, "queue-threshold %u ", info->threshold);
+	if (info->layer != NF_LOG_DEFAULT_LAYER)
+		xt_xlate_add(xl, "nflog-layer %u ", info->layer);
+	xt_xlate_add(xl, "group %u ", info->group);
+}
+
 static int NFLOG_xlate(struct xt_xlate *xl,
 		       const struct xt_xlate_tg_params *params)
 {
@@ -152,23 +248,52 @@ static int NFLOG_xlate(struct xt_xlate *xl,
 	return 1;
 }
 
-static struct xtables_target nflog_target = {
-	.family		= NFPROTO_UNSPEC,
-	.name		= "NFLOG",
-	.version	= XTABLES_VERSION,
-	.size		= XT_ALIGN(sizeof(struct xt_nflog_info)),
-	.userspacesize	= XT_ALIGN(sizeof(struct xt_nflog_info)),
-	.help		= NFLOG_help,
-	.init		= NFLOG_init,
-	.x6_fcheck	= NFLOG_check,
-	.x6_parse	= NFLOG_parse,
-	.print		= NFLOG_print,
-	.save		= NFLOG_save,
-	.x6_options	= NFLOG_opts,
-	.xlate		= NFLOG_xlate,
+static int NFLOG_xlate_v1(struct xt_xlate *xl,
+		       const struct xt_xlate_tg_params *params)
+{
+	const struct xt_nflog_info_v1 *info =
+		(struct xt_nflog_info_v1 *)params->target->data;
+
+	nflog_print_xlate_v1(info, xl, params->escape_quotes);
+
+	return 1;
+}
+
+static struct xtables_target nflog_targets[] = {
+	{
+		.family		= NFPROTO_UNSPEC,
+		.name		= "NFLOG",
+		.version	= XTABLES_VERSION,
+		.size		= XT_ALIGN(sizeof(struct xt_nflog_info)),
+		.userspacesize	= XT_ALIGN(sizeof(struct xt_nflog_info)),
+		.help		= NFLOG_help,
+		.init		= NFLOG_init,
+		.x6_fcheck	= NFLOG_check,
+		.x6_parse	= NFLOG_parse,
+		.print		= NFLOG_print,
+		.save		= NFLOG_save,
+		.x6_options	= NFLOG_opts,
+		.xlate		= NFLOG_xlate,
+	},
+	{
+		.family		= NFPROTO_UNSPEC,
+		.name		= "NFLOG",
+		.version	= XTABLES_VERSION,
+		.revision	= 1,
+		.size		= XT_ALIGN(sizeof(struct xt_nflog_info_v1)),
+		.userspacesize	= XT_ALIGN(sizeof(struct xt_nflog_info_v1)),
+		.help		= NFLOG_help_v1,
+		.init		= NFLOG_init_v1,
+		.x6_fcheck	= NFLOG_check_v1,
+		.x6_parse	= NFLOG_parse,
+		.print		= NFLOG_print_v1,
+		.save		= NFLOG_save_v1,
+		.x6_options	= NFLOG_opts_v1,
+		.xlate		= NFLOG_xlate_v1,
+	}
 };
 
 void _init(void)
 {
-	xtables_register_target(&nflog_target);
+	xtables_register_targets(nflog_targets, ARRAY_SIZE(nflog_targets));
 }

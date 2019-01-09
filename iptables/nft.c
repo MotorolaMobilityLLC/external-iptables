aosp-mirror/platform_external_iptables

commit d4b0d248cc057e39608c7c1c1203dd3f1ea96645

nft: Reduce indenting level in flush_chain_cache()
Instead of doing all in one go, make two separate decisions:

1) If table has no chain cache, either continue or return depending on
   whether we're flushing for a specific table.

2) With chain cache present, flushing strategy once more depends on
   whether we're flushing for a specific table: If given, just remove
   all rules and return. If not, free the cache and set to NULL (so that
   it will be repopulated later), then continue the loop.

Signed-off-by: Phil Sutter <phil@nwl.cc>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

iptables/nft.c
@@ -815,16 +815,20 @@ static void flush_chain_cache(struct nft_handle *h, const char *tablename)
 		if (tablename && strcmp(h->tables[i].name, tablename))
 			continue;
 
-		if (h->table[i].chain_cache) {
-			if (tablename) {
-				nftnl_chain_list_foreach(h->table[i].chain_cache,
-							 __flush_chain_cache, NULL);
-				break;
-			} else {
-				nftnl_chain_list_free(h->table[i].chain_cache);
-				h->table[i].chain_cache = NULL;
-			}
+		if (!h->table[i].chain_cache) {
+			if (tablename)
+				return;
+			continue;
 		}
+
+		if (tablename) {
+			nftnl_chain_list_foreach(h->table[i].chain_cache,
+						 __flush_chain_cache, NULL);
+			return;
+		}
+
+		nftnl_chain_list_free(h->table[i].chain_cache);
+		h->table[i].chain_cache = NULL;
 	}
 }
 

MotorolaMobilityLLC/external-iptables

commit 440af7fbbe51bac9d0084a6efdd945228edc7de3

nft: Simplify flush_chain_cache()
With all the checks for 'tablename' being non-NULL, this code was rather
stupid and really hard to read. And the fix is indeed quite simple: If a
table name was given, use nft_table_builtin_find() and just flush its
chain cache. Otherwise iterate over all builtin tables without any
conditionals for 'tablename'.

Fixes: d4b0d248cc057 ("nft: Reduce indenting level in flush_chain_cache()")
Signed-off-by: Phil Sutter <phil@nwl.cc>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

iptables/nft.c
@@ -793,26 +793,24 @@ static int __flush_chain_cache(struct nftnl_chain *c, void *data)
 
 static void flush_chain_cache(struct nft_handle *h, const char *tablename)
 {
+	const struct builtin_table *table;
 	int i;
+
+	if (tablename) {
+		table = nft_table_builtin_find(h, tablename);
+		if (!table || !h->table[table->type].chain_cache)
+			return;
+		nftnl_chain_list_foreach(h->table[table->type].chain_cache,
+					 __flush_chain_cache, NULL);
+		return;
+	}
 
 	for (i = 0; i < NFT_TABLE_MAX; i++) {
 		if (h->tables[i].name == NULL)
 			continue;
 
-		if (tablename && strcmp(h->tables[i].name, tablename))
+		if (!h->table[i].chain_cache)
 			continue;
-
-		if (!h->table[i].chain_cache) {
-			if (tablename)
-				return;
-			continue;
-		}
-
-		if (tablename) {
-			nftnl_chain_list_foreach(h->table[i].chain_cache,
-						 __flush_chain_cache, NULL);
-			return;
-		}
 
 		nftnl_chain_list_free(h->table[i].chain_cache);
 		h->table[i].chain_cache = NULL;